200f63980853143f14017b50d2b170c95c0f5b06
Raul Dronca <rauldronca@rauls-mbp-2.home>
2019-05-23 10:48:18 +0300

    bug fixes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
 
-    <a href="https://github.com/iRaul/vue-minimal-todo" target="_blank" class="github-corner" aria-label="View source on Github"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#1dd1a1; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>
+    <a href="https://github.com/iRaul/vue-minimal-todo" target="_blank" class="github-corner" aria-label="View source on Github">
+      <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#1dd1a1; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
+        <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
+        <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
+      </svg>
+    </a>
 
     <div class="container mx-auto">
       <div class="mx-auto w-full md:w-1/2 py-8 px-4">
         
         <!-- Title -->
         <div class="flex items-center mb-6">
-          <h1 class="tracking-wide text-white mr-6"><span>MINIMAL</span> TO DO</h1>
+          <h1 class="text-4xl text-white mr-6"><span>MINIMAL</span> TO DO</h1>
 
           <img src="/src/assets/logo.svg" class="hidden md:block" width="40" alt="">
         </div>
@@ -24,21 +29,21 @@
                 class="p-4 mb-4 w-full bg-transparent border-grey-light text-white border rounded">
 
           <transition name="fade">
-            <span class="text-guide text-grey-darker absolute text-xs" v-show="show_guide">Press Enter</span>       
+            <span class="text-guide text-gray-600 absolute text-xs" v-show="show_guide">Press Enter</span>       
           </transition>   
         </div>
       
         <!-- To Do List -->    
         <ul class="list-reset">
           <transition-group name="fade">
-            <li v-for="todo in todos" :key="todo.id"
+            <li v-for="(todo, i) in todos" :key="`${i}-${todo.id}`"
                 class="py-6 px-2 border-b border-grey-darkest flex justify-between items-center relative todo__item">
               <div>
                 <input type="checkbox" :id="todo.id" class="cbx hidden" v-model="todo.completed">
                 <label :for="todo.id" class="text-xl cbx__child"></label>
                 <label :for="todo.id" class="cbx__lbl text-white inline-block mt-1" :class="{ completed: todo.completed }">{{ todo.title }}</label>
               </div>
-              <button v-on:click="removeTodo(todo)" type="button" class="flex items-center delete-button absolute pin-r">
+              <button v-on:click="removeTodo(todo)" type="button" class="flex items-center delete-button absolute right-0">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
               </button>
             </li>          
